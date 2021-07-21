0f4ade4a49f7fbcb84b1dc0cfe41e55d9c13893d
manotik12 <manotik254@gmail.com>
2021-07-22 00:34:50 +0300

    changes to recent

--- src/components/Article.js
+++ src/components/Article.js
@@ -109,6 +109,91 @@ function Article() {
                         <img src="images/logo.png" alt="" />
                     </ArticlePicture>
                 </ArticleCard>
+                <ArticleCard>
+                    <ArticleTextDetails>                        
+                        <Author>
+                            <AuthorProfilePicture>
+                                <img src="/images/logo.png" alt="" />
+                            </AuthorProfilePicture>
+                            <AuthorUserName>
+                                John Doe
+                            </AuthorUserName>
+                        </Author>
+
+                        <ArticleTitle>
+                            Title here
+                        </ArticleTitle>
+
+                        <ArticleSubTitle>
+                            Subtitle here
+                        </ArticleSubTitle>
+
+                        <ArticleFooter>
+                            <ArticleDatePosted>
+                                <p>01/01/2020</p>
+                            </ArticleDatePosted>
+                            <ArticleClassTag>
+                                <p>Class 7</p>
+                            </ArticleClassTag>
+                            <ArticleSubjectTag>
+                                <p>Maths</p>
+                            </ArticleSubjectTag>
+                            <ArticleTopicTag>
+                                <p>Division</p>
+                            </ArticleTopicTag>
+                            
+                        </ArticleFooter>
+                        
+                    </ArticleTextDetails>
+
+                    <ArticlePicture>
+                        <img src="images/logo.png" alt="" />
+                    </ArticlePicture>
+                </ArticleCard>
+                <ArticleCard>
+                    <ArticleTextDetails>                        
+                        <Author>
+                            <AuthorProfilePicture>
+                                <img src="/images/logo.png" alt="" />
+                            </AuthorProfilePicture>
+                            <AuthorUserName>
+                                John Doe
+                            </AuthorUserName>
+                        </Author>
+
+                        <ArticleTitle>
+                            Title here
+                        </ArticleTitle>
+
+                        <ArticleSubTitle>
+                            Subtitle here
+                        </ArticleSubTitle>
+
+                        <ArticleFooter>
+                            <ArticleDatePosted>
+                                <p>01/01/2020</p>
+                            </ArticleDatePosted>
+                            <ArticleClassTag>
+                                <p>Class 7</p>
+                            </ArticleClassTag>
+                            <ArticleSubjectTag>
+                                <p>Maths</p>
+                            </ArticleSubjectTag>
+                            <ArticleTopicTag>
+                                <p>Division</p>
+                            </ArticleTopicTag>
+                            
+                        </ArticleFooter>
+                        
+                    </ArticleTextDetails>
+
+                    <ArticlePicture>
+                        <img src="images/logo.png" alt="" />
+                    </ArticlePicture>
+                </ArticleCard>
+
+
+
 
 
                 
