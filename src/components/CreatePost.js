e8388002f6999b06b9b07f2ca0c50855083c5d94
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-07-21 21:08:28 +0300

    done the UI and some logic for write blog - can read values from the input fields

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import styled from 'styled-components'
 import Header from './Header'
 // npm install draft-js
@@ -7,6 +7,46 @@ import { Editor , EditorState } from 'draft-js'
 // import RichTextEditor from 'react-rte'
 
 export default function CreatePost() {
+
+    const [heading, setHeading] = useState("")
+    const [subHeading, setSubHeading] = useState("")
+    const [topic, setTopic] = useState("")
+    const [level, setLevel] = useState("primary")
+    const [subject, setSubject] = useState("maths")
+    const [Bclass, setClass] = useState("class 1")
+    const [blog, setBlog] = useState("")
+
+    const headingRef = useRef()
+    const subHeadingRef = useRef()
+    const topicRef = useRef()
+    const blogRef = useRef()
+
+    const handleLevelChange = (e) =>{
+        setLevel(e.target.value)
+        // setClass(e.target.value)
+    }
+    const handleClassChange = (e) =>{
+        setClass(e.target.value)
+        // setClass(e.target.value)
+    }
+    const handleSubjectChange = (e) =>{
+        setSubject(e.target.value)
+        // setClass(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // console.log('Level Chosen is: ' + level);
+        // console.log('Your class is: ' + Bclass);
+        // console.log('Your Subject is: ' + subject);
+        setHeading(headingRef.current.value)
+        setSubHeading(subHeadingRef.current.value)
+        setTopic(topicRef.current.value)
+        setBlog(blogRef.current.value)
+
+        console.log(heading, subHeading, topic)
+        console.log(blog)
+    }
 
 
     const [editorState, setEditorState] = useState(
@@ -20,36 +60,104 @@ export default function CreatePost() {
             <Header/> 
 
             <WritePostContainer>
+                <form onSubmit={handleSubmit}>
+
+                
                 
                     <TitleInput>
-                        <input type="text" placeholder="Blog Title"></input>
+                        <input type="text" placeholder="Blog Title" ref={headingRef} ></input>
                     </TitleInput>
 
                     <SubTitleInput>
-                        <input type="text" placeholder="Blog SubTitle"></input>
+                        <input type="text" placeholder="Blog SubTitle" ref={subHeadingRef} ></input>
                     </SubTitleInput>
 
-                    <BlogLevel>
-                        <input type="text" placeholder="Primary/Highchool"></input>
-                    </BlogLevel>
+                    <Horizontal>
 
-                    <BlogClass>
-                        <input type="text" placeholder="Class"></input>
-                    </BlogClass>
+                    
 
-                    <BlogSubject>
-                        <input type="text" placeholder="Subject"></input>
-                    </BlogSubject>
+                    <DropDown>
+                        <BlogLevel> <p>Level</p>
+                            <select value={level} onChange={handleLevelChange}>
+                                <option value="primary">Primary</option>
+                                <option value="highschool">Highschool</option>
+                            </select>
+                        </BlogLevel>
 
-                    <BlogSubjectTopic>
-                        <input type="text" placeholder="Topic"></input>
-                    </BlogSubjectTopic>
+                        <BlogClass> <p>Class</p>                            
+                            <select value={Bclass} onChange={handleClassChange}>
+                                {
+                                level === "primary" ? 
+                                    <>
+                                    <option value="class 1">Class 1</option>
+                                    <option value="class 2">Class 2</option>
+                                    <option value="class 3">Class 3</option>
+                                    <option value="class 4">Class 4</option>
+                                    <option value="class 5">Class 5</option>
+                                    <option value="class 6">Class 6</option>
+                                    <option value="class 7">Class 7</option>
+                                    <option value="class 8">Class 8</option>
+                                    </>
+                                    :
+                                    <>
+                                    <option value="form 1">Form 1</option>
+                                    <option value="form 2">Form 2</option>
+                                    <option value="form 3">Form 3</option>
+                                    <option value="form 4">Form 4</option>
+                                    </>
+                                }
+                            </select> 
+                        </BlogClass>
+
+                        <BlogSubject> <p>Subject</p>
+                            <select value={subject} onChange={handleSubjectChange}>
+                            {
+                                level === "primary" ? 
+                                    <>
+                                    <option value="maths">Maths</option>
+                                    <option value="english">English</option>
+                                    <option value="kiswahili">Kiswahili</option>
+                                    <option value="science">Science</option>
+                                    <option value="social studies">Social Studies</option>
+                                    <option value="religious education">RE</option>
+                                    <option value="other">Other</option>
+                                    </>
+                                    :
+                                    <>
+                                    <option value="maths">Maths</option>
+                                    <option value="english">English</option>
+                                    <option value="kiswahili">Kiswahili</option>
+                                    <option value="biology">Biology</option>
+                                    <option value="chemistry">Chemistry</option>
+                                    <option value="physics">Physics</option>
+                                    <option value="history">History</option>
+                                    <option value="religious education">RE</option>
+                                    <option value="other">Other</option>
+                                    </>
+                                }
+                            </select>
+                        </BlogSubject>
+                        </DropDown>
+
+                        <BlogSubjectTopic>
+                            <p>Topic</p>
+                            <input type="text" placeholder="Topic" ref={topicRef} ></input>
+                        </BlogSubjectTopic>
+                    
+                    </Horizontal>
+
+                    
 
                     <BlogContent>
-                        <Editor className="editor" editorState={editorState} onChange={setEditorState} placeholder="hello"/>
+                        {/* <Editor className="editor" editorState={editorState} onChange={setEditorState} placeholder="hello"/> */}
                         {/* <RichTextEditor/> */}
+                        <textarea rows="20" columns="80" placeholder="Write your Blog here ..." ref={blogRef} ></textarea>
                     </BlogContent>
 
+                    <PostButton>
+                        <input className="submit" type="submit" value="submit"></input>
+                    </PostButton>
+                    </form>
             </WritePostContainer>
         </Container>
     )
@@ -66,8 +174,9 @@ const WritePostContainer = styled.div`
 const TitleInput = styled.div`
     /* width: 100%; */
     /* border: 1px solid grey; */
-    display: flex;
-    justify-content: center;
+    /* display: flex;
+    justify-content: center; */
+    margin-bottom: 10px;
 
     input{
         width: 60%;
@@ -82,6 +191,8 @@ const TitleInput = styled.div`
     
 `
 const SubTitleInput = styled.div`
+    display: flex;
+
     input{
         width: 100%;
         font-size: 18px;
@@ -91,22 +202,57 @@ const SubTitleInput = styled.div`
         }
     } 
 `
+const Horizontal = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    /* border: 1px solid grey; */
+`
+const DropDown = styled.div`
+    display: flex;
+    /* border: 1px solid grey; */
+    justify-content: space-between;
+    width: 50%;
+    flex-wrap: wrap;
+    margin: 10px 0;
+`
 const BlogLevel = styled.div`
-
+    display: flex;
+    p{
+        margin-right: 10px;
+    }
 `
 const BlogClass = styled.div`
-
+    display: flex;
+    p{
+        margin-right: 10px;
+    }
 `
 const BlogSubject = styled.div`
+    display: flex;
+    p{
+        margin-right: 10px;
+    }
 
+    textarea{
+        width: 100%;
+    }
 `
 const BlogSubjectTopic = styled.div`
-
+    display: flex;
+    width: 40%;
+    margin-left: 40px;
+    p{
+        margin-right: 10px;
+    }
+    input{
+        width: 100%;
+    }
 `
 const BlogContent = styled.div`
-    /* height: 400px; */
-    border: 1px solid grey;
     overflow-y: scroll;
+    display: flex;
     
     border: 1px solid grey;
         padding: 30px;
@@ -116,8 +262,7 @@ const BlogContent = styled.div`
         display: none;
     }
 
-    .editor{
-        /* height: 100%; */
+    textarea{
         width: 100%;
         bottom: 0;
         border: 1px solid grey;
@@ -127,4 +272,8 @@ const BlogContent = styled.div`
             outline: none;
         }
     }
+`
+const PostButton = styled.div`
+    margin-top: 20px;
+    text-align: center;
 `
